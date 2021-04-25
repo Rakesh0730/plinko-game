f6e6071e758c250a1d5ce7ebca0c7efece3b8964
Rakesh0730 <70564530+Rakesh0730@users.noreply.github.com>
2021-04-25 14:11:19 +0530

    Update Particle.js

--- Particle.js
+++ Particle.js
@@ -1,13 +1,14 @@
 class Particle {
-    constructor(x, y,r) {
+    constructor(x, y, r) {
 
-        var options ={
+        var options = 
+        {
             restitution:0.4
         }
-        this.r=r;
+        this.r = r;
       
         this.body = Bodies.circle(x, y, this.r,options);       
-        this.color=color(random(0, 255), random(0, 255), random(0, 255));
+        this.color = color(random(0, 255), random(0, 255), random(0, 255));
         World.add(world, this.body);
 
     }
